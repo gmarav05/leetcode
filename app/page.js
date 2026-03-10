522bb9529890e2d38b3d37f50688198113ee8806
Aravind <gmarav005@gmail.com>
2026-03-10 18:32:51 +0530

    Add clerk auth with github
    Signed-off-by: Aravind <gmarav005@gmail.com>

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import { Button } from "../components/ui/button";
+import { User } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>
-        Click me
-      </Button>
+      <UserButton/>
     </div>
   );
 }
